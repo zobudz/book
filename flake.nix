0c276a75af9f4976220abff42091cdfe4680ef0b
Nigel Bray <NigelBray_hacking_on_zobudz_book@aeroplaying.uk>
2025-01-05 18:35:40 +0000

    steps to publish

--- flake.nix
+++ flake.nix
@@ -1,3 +1,7 @@
+# https://stackoverflow.com/questions/600079/how-do-i-clone-a-subdirectory-only-of-a-git-repository/52269934#52269934
+# git clone -n --depth=1 --filter=tree:0  https://github.com/zobudz/book; cd book/; git sparse-checkout set --no-cone  /book; git checkout 
+# ssh root@to1.uk -- "cd /var/www/to1.uk/gits/book/; git checkout"
+
 {
   description = "xobudz/book: Electronic airprox avoidance";
 
